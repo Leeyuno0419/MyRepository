974f20d3edbc5939404de9f1737c0f5a98242c73
LEEYUNO <LEEYUNO@DESKTOP-OVBT00H>
2023-12-18 17:47:14 +0900

    Fitst A, B, C Classes

---
+++ MyGit/mygit/A.java
@@ -0,0 +1,7 @@
+package mygit;
+
+public class A {
+	public static void main(String[] args) {
+		ptintln("This is AClass");
+	}
+}
---
+++ MyGit/mygit/B.java
@@ -0,0 +1,7 @@
+package mygit;
+
+public class B {
+	public static void main(String[] args) {
+		ptintln("This is BClass");
+	}
+}
